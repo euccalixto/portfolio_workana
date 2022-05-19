f9adfb7933da616d7c71b25381f0c4dd1c8ffa84
Eduardo Calixto <71821586+educalixto@users.noreply.github.com>
2022-05-19 15:31:42 -0300

    finish responsiveness

--- js/logic/slider_touch.js
+++ js/logic/slider_touch.js
@@ -1,52 +1,82 @@
+document.getElementById('ds')
+let shadow=document.querySelector('.shadows');
+let translate= 0;
+var movements=[];
+let i=0;
+let canMove=false;
+let alternate={state:false,timer:10,i:0};
 document.querySelectorAll('.slider-container').forEach(slider => {
-  let shadow=document.querySelector('.shadows');
-  console.log(slider);
-  let translate= 0;
-  var movements=[];
-  let i=0;
-  let canMove=false;
+  //desktop
+  shadow.onclick = () => {
+    alternate.state= true;
+  }
+  
   shadow.onmousedown = (evt) => {
     canMove=true;
   }
-  shadow.onmouseup = (evt) => {
-    canMove=false;
-    if(slider.classList.contains('not-select')){
-      slider.classList.remove('not-select');
-    }
 
-    if(shadow.classList.contains('grabbing')){
-      shadow.classList.remove('grabbing');
-    }
+  shadow.onmouseup = (evt) => {
+    stopMove(slider);
   }
 
   shadow.onmouseleave = () => {
     if(canMove){
-      canMove=false;
-      if(shadow.classList.contains('grabbing')){
-        shadow.classList.remove('grabbing');
-      }
+      stopMove(slider);
     }
   }
 
   shadow.onmousemove = (evtMove) => {
     if(canMove){
-      // AnimateCardX(slider.getBoundingClientRect(),evtMove.screenX);
-      slider.classList.add('not-select');
-      shadow.classList.add('not-select');
-      shadow.classList.add('grabbing');
-      movements[i]= evtMove.screenX;
-      //console.log(evt);
-      slider.style.transform= `translateX(${translate}px)`;
-      //console.log(evtMove.screenX)
-      //slider.scrollLeft = evtMove.screenX * -1;
-      if(movements[i-1]<movements[i] && slider.getBoundingClientRect().left + 20 < 0){
-        translate+=5;
-      }else if(movements[i-1]>movements[i] && slider.getBoundingClientRect().right - window.innerWidth + 20 > 0){
-        translate-=5;
-      }else{
-        translate+=0;
-      }
-      i++;
+      Move(slider,evtMove,3,true);
     }
   }
+
+  //mobile
+  shadow.ontouchstart = (evt) => {
+    canMove=true;
+  }
+  shadow.ontouchend = (evt) => {
+    stopMove(slider);
+  }
+  shadow.ontouchcancel = (evt) => {
+    stopMove(slider);
+  }
+  shadow.ontouchmove = (evtMove) => {
+    Move(slider,evtMove,0.1,false);
+  }
 });
+
+
+function stopMove(slider){
+  canMove=false;
+
+  if(slider.classList.contains('not-select')){
+    slider.classList.remove('not-select');
+  }
+
+  if(shadow.classList.contains('grabbing')){
+    shadow.classList.remove('grabbing');
+  }
+}
+
+function Move(slider,evtMove,t,stateAlter){
+  // AnimateCardX(slider.getBoundingClientRect(),evtMove.screenX);
+  slider.classList.add('not-select');
+  shadow.classList.add('not-select');
+  shadow.classList.add('grabbing');
+  movements[i]= evtMove.screenX || evtMove.touches[0].screenX;
+  //console.log(evt);
+  slider.style.transform= `translateX(${translate}px)`;
+  //console.log(evtMove.screenX)
+  //slider.scrollLeft = evtMove.screenX * -1;
+  if(movements[i-1]<movements[i] && slider.getBoundingClientRect().left + 20 < 0){
+    translate+=t + (alternate.state && stateAlter) ? alternate.timer : 0;
+    alternate.state= false;
+  }else if(movements[i-1]>movements[i] && slider.getBoundingClientRect().right - window.innerWidth + 20 > 0){
+    translate-=t + (alternate.state && stateAlter) ? alternate.timer : 0;
+    alternate.state=false;
+  }else{
+    translate+=0;
+  }
+  i++;
+}
